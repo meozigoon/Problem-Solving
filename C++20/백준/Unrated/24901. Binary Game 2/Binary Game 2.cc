a85a4fb8fc8d6548b63203a30d677680a96caa47
DH.L <107869892+meozigoon@users.noreply.github.com>
2025-10-16 11:11:46 +0900

    [Unrated] Title: Binary Game 2, Time: 124 ms, Memory: 120356 KB, Score: 222222 point -BaekjoonHub

--- C++20/백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
+++ C++20/백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
@@ -1,33 +1,42 @@
+//; <<DOC
+// \u000a /*
+// \
+let arr = ["0", "01", "0110", "011011", "011011100", "011011100101", "011011100101110", "011011100101110111", "0110111001011101111000", "01101110010111011110001001", "011011100101110111100010011010"]
+// \
+let n = Int(readLine()!)!
+// \
+print(arr[n])
+// \
+/*
 #include <iostream>
-#include <stack>
-
 using namespace std;
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
-    cin >> n;
-    for (int i = 0; i <= n; i++)
-    {
-        stack<int> s;
-        int x = i;
-        while (x != 0)
-        {
-            if (x % 2 == 1)
-                s.push(1);
-            else
-                s.push(0);
-            x /= 2;
-        }
-        if (s.empty())
-            cout << 0;
-        while (!s.empty())
-        {
-            cout << s.top();
-            s.pop();
-        }
-    }
-    return 0;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string arr[] =
+      {"0", "01", "0110", "011011", "011011100", "011011100101", "011011100101110", "011011100101110111", "0110111001011101111000", "01101110010111011110001001", "011011100101110111100010011010"};
+  int n;
+  cin >> n;
+  cout << arr[n];
+  return 0;
 }
+// \
+*/
+// */
+/* \u002a/
+public class Main {
+    public static void main(String[] args) throws java.io.IOException {
+        String arr[] =
+      {"0", "01", "0110", "011011", "011011100", "011011100101", "011011100101110", "011011100101110111", "0110111001011101111000", "01101110010111011110001001", "011011100101110111100010011010"};
+        java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System. in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(arr[n]);
+    }
+}
+// */
+/*
+DOC
+arr = ["0", "01", "0110", "011011", "011011100", "011011100101", "011011100101110", "011011100101110111", "0110111001011101111000", "01101110010111011110001001", "011011100101110111100010011010"]
+n = gets.to_i
+print(arr[n])
+/0 */
